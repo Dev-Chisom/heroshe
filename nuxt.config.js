f57dbebfee77af09878add5e8b7f1cff86a90fad
Onwugbenu Chisom <onwugbenuglory@gmail.com>
2021-09-09 07:23:37 +0100

    fix deployment issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,11 @@
+require('dotenv').config()
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+  ssr: false,
+  generate:{
+    fallback: true
+  },
   head: {
     title: "frontend_test",
     htmlAttrs: {
